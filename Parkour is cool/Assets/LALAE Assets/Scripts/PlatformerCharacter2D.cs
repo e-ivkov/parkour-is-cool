62674dda3f141081e05b64737452c9f1d76b07e0
Егор Ивков <e.ivkov@mail.ru>
2017-03-19 13:01:05 +0300

    Guy basic animations
    Now some of the basic animations of a guy are working
    Camera bug fixes
    Camera minor TODO: forbid character going out of the screen

--- Parkour is cool/Assets/LALAE Assets/Scripts/PlatformerCharacter2D.cs
+++ Parkour is cool/Assets/LALAE Assets/Scripts/PlatformerCharacter2D.cs
@@ -21,7 +21,7 @@ namespace UnityStandardAssets._2D
         private float m_RunMultiplier = 1.5f;
 
         private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
-        const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+        const float k_GroundedRadius = .4f; // Radius of the overlap circle to determine if grounded
         private bool m_Grounded;            // Whether or not the player is grounded.
         private Transform m_CeilingCheck;   // A position marking where to check for ceilings
         const float k_CeilingRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
@@ -40,7 +40,7 @@ namespace UnityStandardAssets._2D
         {
             // Setting up references.
             m_GroundCheck = transform.Find("GroundCheck");
-           // m_CeilingCheck = transform.Find("CeilingCheck");
+            m_CeilingCheck = transform.Find("CeilingCheck");
             m_Anim = GetComponent<Animator>();
             m_Rigidbody2D = GetComponent<Rigidbody2D>();
         }
@@ -68,7 +68,7 @@ namespace UnityStandardAssets._2D
         public void Move(float move, bool crouch, bool jump, bool run)
         {
             // If crouching, check to see if the character can stand up
-           /* if (!crouch && m_Anim.GetBool("Crouch"))
+            if (!crouch && m_Anim.GetBool("Crouch"))
             {
                 // If the character has a ceiling preventing them from standing up, keep them crouching
                 if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
@@ -78,13 +78,13 @@ namespace UnityStandardAssets._2D
             }
 
             // Set whether or not the character is crouching in the animator
-            m_Anim.SetBool("Crouch", crouch);*/
+            m_Anim.SetBool("Crouch", crouch);
 
             //only control the player if grounded or airControl is turned on
             if (m_Grounded || m_AirControl)
             {
                 // Reduce the speed if crouching by the crouchSpeed multiplier
-                //move = (crouch ? move * m_CrouchSpeed : move);
+                move = (crouch ? move * m_CrouchSpeed : move);
 
                 // The Speed animator parameter is set to the absolute value of the horizontal input.
                 m_Anim.SetFloat("Speed", Mathf.Abs(move));
@@ -145,13 +145,13 @@ namespace UnityStandardAssets._2D
             }
         }
 
-       /* public void PerformTrick(Trick trick)
+        public void PerformTrick(Trick trick)
         {
             m_LevelXP += trick.m_XP * m_TrickMultipliyer;
             m_TrickMultipliyer *= 2;
             m_Anim.SetTrigger("StartTrick");
             //TODO: create replacement animator, and actually start the trick animation
-        } */
+        }
 
         private void Flip()
         {
