38fb91fbcbdc044d6d42b94e8c34c215211f9d1a
Anton Skudarnov <Anton Skudarnov>
2017-03-26 02:39:15 +0300

    inital state machine

---
+++ Parkour is cool/Assets/LALAE Assets/Scripts/State Machine/CharacterFSM.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+namespace UnityStandardAssets._2D
+{
+    public enum eCharacterState {
+        INIT,
+        IDLE,
+        SLOW_RUN,
+        RUN,
+        JUMP,
+        FALL,
+        TRICK,
+        FAIL
+
+    }
+    public class CharacterFSM: FiniteStateMachine<eCharacterState>
+    {
+
+
+
+        private PlatformerCharacter2D m_Character;
+        private bool m_Jump;
+        
+
+        private void Awake()
+        {
+            m_Character = GetComponent<PlatformerCharacter2D>();
+        }
+
+       
+
+        public CharacterFSM()
+        {
+
+        }
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+           
+        }
+
+
+        void Run()
+        {
+            Debug.Log("RUN");
+        }
+
+        void SlowRun() {
+            Debug.Log("SLOWAWWWRUN");
+        }
+    }
+}
